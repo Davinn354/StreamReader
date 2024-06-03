2de863ed30e0f43e1331af736e489cfaebdaa1fa
Davinn354 <daviipad53@gmail.com>
2024-06-03 14:44:54 +0700

    Penambahan int main yang isinya string baris, string NamaFile beserta cout dan cin, penambahan ofstream untuk membuka file dalam mode menulis dan outfile untuk menunjuk ke sebuah nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -2,3 +2,19 @@
 #include <iostream>
 #include<string>
 using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    // Membuka file dalam mode menulis.
+    ofstream outfile;
+    // Menunjuk ke sebuah nama file
+    outfile.open(NamaFile = ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+}
