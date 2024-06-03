8c492ac3fd385806bb1f25b78270b1725f674561
Davinn354 <daviipad53@gmail.com>
2024-06-03 14:33:24 +0700

    Penambahan if serta while if jika file nya ada makan dan while melakukan perulangan setiap baris serta cout untuk menampilkan

--- ioFile.cpp
+++ ioFile.cpp
@@ -23,4 +23,23 @@ int main() {
         // menulis dan memasukan nilai dari 'baris' kedalam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka filedalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contogfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+    }
 }
