73f8f42f4522eaac2cc0dfa55b6f5a3c3d164e03
Davinn354 <daviipad53@gmail.com>
2024-06-03 14:37:09 +0700

    Penambahan else cout jika file tidak ditemukan maka tulisan unable to open file akan muncul

--- ioFile.cpp
+++ ioFile.cpp
@@ -44,4 +44,7 @@ int main() {
         //tutup file tersebut setelah selesai
         infile.close();
     }
+    //jika tidak ditemukan fille maka akan menampilkan ini
+    else cout << "Unable to open file ";
+    return 0;
 }
